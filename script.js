ebf4a044c4319d0ec1d5ef4508e6c64f9e974886
milosdimi <132084913+milosdimi@users.noreply.github.com>
2024-06-08 18:38:55 +0200

    farbe hinzugefügt

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-window.onscroll = function() {
+//window.onscroll = function() {
     let shoppingCart = document.getElementById('shoppingCart');
     if(window.scrollY > 0) {
         shoppingCart.style = 'top: 0';
     } else {
         shoppingCart.style = 'top: 100px';
     }
-}
+//}
